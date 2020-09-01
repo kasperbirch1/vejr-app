3c7496728264bf6b9daa0348b39d72d7dfea3bfb
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-09-01 10:29:49 +0200

    updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-vejr-app`,
+        short_name: `vejrapp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `fullscreen`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
