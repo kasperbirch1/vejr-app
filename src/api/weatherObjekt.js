e9b9bc6ad549c2b40c3b364901dcae57f12ad51c
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-09-21 09:53:51 +0200

    updatet weatherObjekt: add Fog

--- src/api/weatherObjekt.js
+++ src/api/weatherObjekt.js
@@ -40,6 +40,47 @@ export const weatherObjekt = {
             }
         ],
     },
+    Fog: {
+        icon: "🌫️",
+        activities: [
+            {
+                img: "https://cf.bstatic.com/images/hotel/max1024x768/220/220680927.jpg",
+                title: "Fog Strandtur",
+                location: "kysten / stranden",
+                price: 0,
+            },
+            {
+                img: "https://images.interactives.dk/istock-498375904-zn2yw_t3xRkfwd-KWXd9Lg.jpg?auto=compress&ch=Width%2CDPR&dpr=2.63&ixjsv=2.2.4&q=38&rect=0%2C0%2C0%2C0&w=430",
+                title: "Fog cykkeltur",
+                location: "landevejen",
+                price: 0,
+            },
+            {
+                img: "https://www.kbhmuseer.dk/wp-content/uploads/2019/10/isbjorn_foto_henrik_sorensen-wpcf_773x435.jpg",
+                title: "Fog zoo",
+                location: "frederiksberg",
+                price: 100,
+            },
+
+        ],
+        music: [
+            {
+                coverImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFYdzLkxTCFM3v8SfoHlYSGkIDc_nogsK5lA&usqp=CAU",
+                songTitle: "Fog song1",
+                artist: "artist1"
+            },
+            {
+                coverImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFYdzLkxTCFM3v8SfoHlYSGkIDc_nogsK5lA&usqp=CAU",
+                songTitle: "Fog song2",
+                artist: "artist2"
+            },
+            {
+                coverImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFYdzLkxTCFM3v8SfoHlYSGkIDc_nogsK5lA&usqp=CAU",
+                songTitle: "Fog song3",
+                artist: "artist3"
+            }
+        ],
+    },
     Thunderstorm: {
         icon: "🌩️",
         activities: [
